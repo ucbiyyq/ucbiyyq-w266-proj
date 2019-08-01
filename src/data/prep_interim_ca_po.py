a07aff9194c0f035237130bff4c3b6522202b1bf
ucbiyyq <yangyang.qian@berkeley.edu>
2019-08-01 02:54:39 +0000

    rescued more files

--- src/data/prep_interim_ca_po.py
+++ src/data/prep_interim_ca_po.py
@@ -1,11 +1,16 @@
 import pandas as pd
+import janitor
 
-def prep_interim_data(p_ca_po):
+def prep_interim_data(p_in, p_out):
     '''
     helper function to prepare the interim dataset
     intention is to mimic the catalog data at the Employer
     we don't need much of the data, just the unspsc codes and the item names
     '''
-    ca_po = pd.read_csv(p_ca_po)
-    print("binky")
-    return(ca_po)
+    t1 = pd.read_csv(p_in, dtype=str).clean_names()
+    t2 = t1[["normalized_unspsc", "item_name"]]
+    t3 = t2.drop_duplicates().dropna()
+    t4 = t3.sort_values(by=["normalized_unspsc", "item_name"]).reset_index(drop = True)
+    t4.to_csv(p_out)
+    print("interim data saved to ", p_out)
+    
