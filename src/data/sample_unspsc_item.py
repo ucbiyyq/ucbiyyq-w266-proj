282eca0e41b44fd327b3555c26a651c82d8cffd3
ucbiyyq <yangyang.qian@berkeley.edu>
2019-08-02 06:15:34 +0000

    more rescues

--- src/data/sample_unspsc_item.py
+++ src/data/sample_unspsc_item.py
@@ -1,19 +1,17 @@
 import pandas as pd
 import janitor
 
-def sample_unspsc_item(p_in, samples_per_group = 1, samples_overall = 100, random_state = 0):
+def sample_unspsc_item(unspsc_items, samples_per_group = 1, samples_overall = 100, random_state = 0):
     '''
     helper function to sample samples_per_group number of examples per unspsc code,
     and samples_overall number of codes overall
     
     params
-        p_in
-            path to the unspsc interim file, i.e.
-            data/interim/unspsc-item.csv
+        unspsc_items
+            the data frame that contains the unspsc_items
     '''
-    unspsc_item = pd.read_csv(p_in, dtype=str).clean_names()
-    t1 = unspsc_item.groupby("normalized_unspsc")
-    t2 = t1.apply(lambda x: x.sample(n = samples_per_group, random_state = 0))
+    t1 = unspsc_items.groupby("normalized_unspsc")
+    t2 = t1.apply(lambda x: x.sample(n = samples_per_group, random_state = 0, replace = True))
     t3 = t2.reset_index(drop  = True)
     t4 = t3.sample(n = samples_overall, random_state = 0).sort_values(by = "item_name").reset_index(drop  = True)
     results = t4
